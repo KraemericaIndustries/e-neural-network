8b4805551d091836e00a7d6dfb717018a8221637
Bob Kraemer <bob.kraemer.android@gmail.com>
2024-01-03 16:35:41 -0500

    Verified code working through lecture 8: "Coding Perceptron"

--- src/main/java/kraemericaIndustries/neuralnetwork/App.java
+++ src/main/java/kraemericaIndustries/neuralnetwork/App.java
@@ -1,13 +1,22 @@
 package kraemericaIndustries.neuralnetwork;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+//  A simple perceptron    
+	public static void main( String[] args )
     {
-        System.out.println( "Hello GitHub PAT!" );
+    	double[] x = {0, 1};	  //  Our inputs
+        double[] w = {0.5, 0.5};  //  Our weights
+        double b = 0.5;			  //  Our bias
+        double z = 0.0;			  //  Our weighted sum
+        
+        for(int i =0; i < x.length; i++) {  //  SUM of PRODUCTS of INPUTS and WEIGHTS
+        	z += x[i] * w[i];
+        }
+        
+        z += b;  //  INCLUDE any bias
+
+        double a = z > 0 ? 1.0: 0.0;  //  Our activation function... IF (z > 0), THEN (a = 0.0), ELSE (a = 1.0)
+        System.out.println(a);		  //  Our output
     }
 }
