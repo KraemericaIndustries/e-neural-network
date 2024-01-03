43a7569d713a74f99d20f8f883a7a282fd26c66d
Bob Kraemer <bob.kraemer.android@gmail.com>
2024-01-03 17:45:36 -0500

    Verified code working through lecture 12: "Perceptron AND"

--- src/main/java/kraemericaIndustries/neuralnetwork/App.java
+++ src/main/java/kraemericaIndustries/neuralnetwork/App.java
@@ -1,22 +1,37 @@
 package kraemericaIndustries.neuralnetwork;
 
-public class App 
-{
-//  A simple perceptron    
-	public static void main( String[] args )
-    {
-    	double[] x = {0, 1};	  //  Our inputs
-        double[] w = {0.5, 0.5};  //  Our weights
-        double b = 0.5;			  //  Our bias
-        double z = 0.0;			  //  Our weighted sum
-        
-        for(int i =0; i < x.length; i++) {  //  SUM of PRODUCTS of INPUTS and WEIGHTS
-        	z += x[i] * w[i];
-        }
-        
-        z += b;  //  INCLUDE any bias
 
-        double a = z > 0 ? 1.0: 0.0;  //  Our activation function... IF (z > 0), THEN (a = 0.0), ELSE (a = 1.0)
-        System.out.println(a);		  //  Our output
-    }
+public class App {
+
+	static double neuron(double[] x, double[] w, double b) {  //  x = inputs, w = weights, b = bias
+
+		double z = 0.0;  //  Our weighted sum
+
+		for (int i = 0; i < x.length; i++) {  //  SUM of PRODUCTS of INPUTS and WEIGHTS
+			z += x[i] * w[i];
+		}
+
+		z += b;  //  INCLUDE any bias
+		return z > 0 ? 1.0 : 0.0;  //  Our activation function... IF (z > 0), THEN (a = 0.0), ELSE (a = 1.0)
+	}
+
+	static double and(double x1, double x2) {
+    	//  RETURN the result of INVOKING neuron()
+		//  ARGUMENTS for neuron() are inputs of x1 & x2, weights[] of 1 & 1, and a bias that achieves the desired result of an AND gate: 
+		return neuron(new double[] {x1, x2}, new double[] {1, 1}, -1);  
+	}
+
+	public static void main(String[] args) {
+
+		for(int i = 0; i < 4; i++) {  //  ITERATE over all possible combinations for 2 inputs...
+			double x1 = i/2;		  //  The next 2 lines achieve the possible input combinations of 00, 01, 10, 11
+			double x2 = i%2;
+
+			double output = and(x1, x2);  //  INVOKE and()... first iteration x1 = 0, x2 = 0...
+
+     		//  Our output.  (%d = integer), (\t = tab), (\n = newline)
+			//  Doubles x1, x2, output are cast to ints
+			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);  
+		}
+	}
 }
