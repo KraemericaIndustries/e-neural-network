e918f32ff6ad77e3783d161af21ec7f115f7d74b
Bob Kraemer <bob.kraemer.android@gmail.com>
2024-01-04 08:25:17 -0500

    Tweaked lecture 13: "OR, NOR, NAND" to produce truth table for all gates

--- src/main/java/kraemericaIndustries/neuralnetwork/App.java
+++ src/main/java/kraemericaIndustries/neuralnetwork/App.java
@@ -28,8 +28,9 @@ public class App {
 	
 	static double xor(double x1, double x2) {
     	//  RETURN the result of INVOKING neuron()
-		//  ARGUMENTS for neuron() are inputs of x1 & x2, weights[] of 1 & 1, and a bias that achieves the desired result of an XOR gate: 
-		return neuron(new double[] {x1, x2}, new double[] {1, 1}, -1);  
+		//  ARGUMENTS for neuron() are inputs of x1 & x2, weights[] of 1 & 1, and a bias that achieves the desired result of an XOR gate
+		//  XOR is achieved by ANDing the results of OR and NAND:
+		return and(or(x1, x2), nand(x1, x2));  
 	}
 	
 	static double nor(double x1, double x2) {
@@ -46,26 +47,72 @@ public class App {
 	
 	static double xnor(double x1, double x2) {
     	//  RETURN the result of INVOKING neuron()
-		//  ARGUMENTS for neuron() are inputs of x1 & x2, weights[] of 1 & 1, and a bias that achieves the desired result of an XNOR gate: 
-		return neuron(new double[] {x1, x2}, new double[] {1, 1}, -1);  
+		//  ARGUMENTS for neuron() are inputs of x1 & x2, weights[] of 1 & 1, and a bias that achieves the desired result of an XNOR gate
+		//  XNOR is achieved by ORing the results of AND and NOR:
+		return or(and(x1, x2), nor(x1, x2));  
 	}
 
 	public static void main(String[] args) {
 
+		System.out.println("AND:");
 		for(int i = 0; i < 4; i++) {  //  ITERATE over all possible combinations for 2 inputs...
 			double x1 = i/2;		  //  The next 2 lines achieve the possible input combinations of 00, 01, 10, 11
 			double x2 = i%2;
-
 			double output = and(x1, x2);  //  INVOKE and()... first iteration x1 = 0, x2 = 0...
-//			double output = or(x1, x2);  //  INVOKE and()... first iteration x1 = 0, x2 = 0...
-//			double output = xor(x1, x2);  //  INVOKE and()... first iteration x1 = 0, x2 = 0...   //  WIP
-//			double output = nor(x1, x2);  //  INVOKE and()... first iteration x1 = 0, x2 = 0...
-//			double output = nand(x1, x2);  //  INVOKE and()... first iteration x1 = 0, x2 = 0...
-//			double output = xnor(x1, x2);  //  INVOKE and()... first iteration x1 = 0, x2 = 0...  //  WIP
+			
+			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);  //  Our output.  (%d = integer), (\t = tab), (\n = newline).  Doubles x1, x2, output are cast to ints  
+		}
+		System.out.println();
+		
+		System.out.println("OR:");
+		for(int i = 0; i < 4; i++) {  //  ITERATE over all possible combinations for 2 inputs...
+			double x1 = i/2;		  //  The next 2 lines achieve the possible input combinations of 00, 01, 10, 11
+			double x2 = i%2;
+			double output = or(x1, x2);  //  INVOKE or()... first iteration x1 = 0, x2 = 0...
 
-     		//  Our output.  (%d = integer), (\t = tab), (\n = newline)
-			//  Doubles x1, x2, output are cast to ints
-			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);  
+			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);  //  Our output.  (%d = integer), (\t = tab), (\n = newline).  Doubles x1, x2, output are cast to ints  
+		}
+		System.out.println();
+		
+		System.out.println("XOR:");
+		for(int i = 0; i < 4; i++) {  //  ITERATE over all possible combinations for 2 inputs...
+			double x1 = i/2;		  //  The next 2 lines achieve the possible input combinations of 00, 01, 10, 11
+			double x2 = i%2;
+			double output = xor(x1, x2);  //  INVOKE xor()... first iteration x1 = 0, x2 = 0...
+
+			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);  //  Our output.  (%d = integer), (\t = tab), (\n = newline).  Doubles x1, x2, output are cast to ints  
+		}
+		System.out.println();		
+		
+		
+		System.out.println("NOR:");
+		for(int i = 0; i < 4; i++) {  //  ITERATE over all possible combinations for 2 inputs...
+			double x1 = i/2;		  //  The next 2 lines achieve the possible input combinations of 00, 01, 10, 11
+			double x2 = i%2;
+			double output = nor(x1, x2);  //  INVOKE nor()... first iteration x1 = 0, x2 = 0...
+
+			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);  //  Our output.  (%d = integer), (\t = tab), (\n = newline).  Doubles x1, x2, output are cast to ints  
+		}
+		System.out.println();
+		
+		System.out.println("NAND:");
+		for(int i = 0; i < 4; i++) {  //  ITERATE over all possible combinations for 2 inputs...
+			double x1 = i/2;		  //  The next 2 lines achieve the possible input combinations of 00, 01, 10, 11
+			double x2 = i%2;
+			double output = nand(x1, x2);  //  INVOKE nand()... first iteration x1 = 0, x2 = 0...
+
+			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);  //  Our output.  (%d = integer), (\t = tab), (\n = newline).  Doubles x1, x2, output are cast to ints  
+		}
+		System.out.println();
+		
+		
+		System.out.println("XNOR:");
+		for(int i = 0; i < 4; i++) {  //  ITERATE over all possible combinations for 2 inputs...
+			double x1 = i/2;		  //  The next 2 lines achieve the possible input combinations of 00, 01, 10, 11
+			double x2 = i%2;
+			double output = xnor(x1, x2);  //  INVOKE and()... first iteration x1 = 0, x2 = 0...
+
+			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);  //  Our output.  (%d = integer), (\t = tab), (\n = newline).  Doubles x1, x2, output are cast to ints  
 		}
 	}
 }
