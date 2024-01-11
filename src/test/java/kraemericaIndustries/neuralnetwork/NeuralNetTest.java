bd9aeb4f9e864ed617f4e9499a962813f1b5aabf
Bob Kraemer <bob.kraemer.android@gmail.com>
2024-01-11 10:00:46 -0500

    Verified code working through lecture 44: "Multiple Columns of Input"

--- src/test/java/kraemericaIndustries/neuralnetwork/NeuralNetTest.java
+++ src/test/java/kraemericaIndustries/neuralnetwork/NeuralNetTest.java
@@ -1,5 +1,7 @@
 package kraemericaIndustries.neuralnetwork;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import kraemericaIndustries.matrix.Matrix;
@@ -7,18 +9,18 @@ import kraemericaIndustries.matrix.Matrix;
 public class NeuralNetTest {
 
 	@Test
-	public void test() {
+	public void testAddBias() {
 		
-		Matrix input = new Matrix(3, 1, i -> (i + 1));
+		Matrix input = new Matrix(3, 3, i -> (i + 1));
 		Matrix weights = new Matrix(3, 3, i -> (i + 1));
 		Matrix biases = new Matrix(3, 1, i -> (i + 1));
 		
 		Matrix result = weights.multiply(input).modify((row, col, value) -> value + biases.get(row));
 		
-		System.out.println(input);
-		System.out.println(weights);
-		System.out.println(biases);
-		System.out.println(result);
+		double[] expectedValues = {+31.00000, +37.00000, +43.00000, +68.00000, +83.00000, +98.00000, +105.00000, +129.00000, +153.00000 };
+		
+		Matrix expected = new Matrix(3, 3, i -> expectedValues[i]);
+		
+		assertTrue(expected.equals(result));
 	}
-
 }
