fcc672073b17053bd6ffff38d74fef4d06f738df
Bob Kraemer <bob.kraemer.android@gmail.com>
2024-01-17 10:57:33 -0500

    Completed code through lecture 71: "Implementing Cross Entropy"

---
+++ src/main/java/kraemericaIndustries/neuralnetwork/LossFunction.java
@@ -0,0 +1,12 @@
+package kraemericaIndustries.neuralnetwork;
+
+import kraemericaIndustries.matrix.Matrix;
+
+public class LossFunction {
+
+	public static Matrix crossEntropy(Matrix expected, Matrix actual) {
+		return actual.apply((index, value) -> {
+			return -expected.get(index) * Math.log(value);
+		}).sumColumns();
+	}
+}
