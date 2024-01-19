d137a42a78cee42670f00499eda8840d3c7ddce3
Bob Kraemer <bob.kraemer.android@gmail.com>
2024-01-19 08:49:30 -0500

    Verified code working through lecture 83: "Approximator".  BEWARE of extra space in output (which I can not seem to rectify)!

--- src/test/java/kraemericaIndustries/neuralnetwork/NeuralNetTest.java
+++ src/test/java/kraemericaIndustries/neuralnetwork/NeuralNetTest.java
@@ -13,6 +13,20 @@ public class NeuralNetTest {
 	private Random random = new Random();
 	
 	@Test
+	public void testApproximator() {
+		
+		final int rows = 4;
+		final int cols = 5;
+		
+		Matrix input = new Matrix(rows, cols, i -> random.nextGaussian());
+		
+		Approximator.gradient(input, null);
+		
+		System.out.println();
+		System.out.println(input);
+	}
+	
+	@Test
 	public void testCrossEntropy() {
 		
 		double[] expectedValues = {1, 0, 0, 0, 0, 1, 0, 1, 0};
