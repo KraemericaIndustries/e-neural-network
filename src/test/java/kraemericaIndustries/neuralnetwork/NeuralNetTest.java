55cabb214ae21db8d85b291e8bc438023863dba8
Bob Kraemer <bob.kraemer.android@gmail.com>
2024-01-23 08:37:40 -0500

    Verified code working through lecture 105: "Completing the Weights Backprop Test"

--- src/test/java/kraemericaIndustries/neuralnetwork/NeuralNetTest.java
+++ src/test/java/kraemericaIndustries/neuralnetwork/NeuralNetTest.java
@@ -24,7 +24,7 @@ public class NeuralNetTest {
 		Matrix expected = new Matrix(outputRows, cols, i -> 0);
 		
 		Matrix weights = new Matrix(outputRows, inputRows, i -> random.nextGaussian());
-		Matrix biases = new Matrix(outputRows, inputRows, i -> random.nextGaussian());
+		Matrix biases = new Matrix(outputRows, 1, i -> random.nextGaussian());
 		
 		for(int col = 0; col < cols; col++) {
 			int randomRow = random.nextInt(outputRows);
@@ -32,13 +32,15 @@ public class NeuralNetTest {
 			expected.set(randomRow, col, 1);
 		}
 		
-		Matrix softmaxOutput = input.softmax();
+		Matrix softmaxOutput = weights.multiply(input).modify((row, col, value) -> value + biases.get(row)).softmax();
 		
 		Matrix approximatedResult = Approximator.gradient(input, in -> {
+			in = weights.multiply(in).modify((row, col, value) -> value + biases.get(row));
 			return LossFunction.crossEntropy(expected, in.softmax());
 		});
 		
 		Matrix calculatedResult = softmaxOutput.apply((index, value) -> value - expected.get(index));
+		calculatedResult = weights.transpose().multiply(calculatedResult);
 		
 		assertTrue(approximatedResult.equals(calculatedResult));
 	}
