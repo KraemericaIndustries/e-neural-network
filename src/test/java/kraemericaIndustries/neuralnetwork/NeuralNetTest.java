f4e602f4d89ef6f89111010fb532f53de33a8217
Bob Kraemer <bob.kraemer.android@gmail.com>
2024-01-25 10:27:28 -0500

    Completed code through lecture 130: "130. Generating Trainable Data"

--- src/test/java/kraemericaIndustries/neuralnetwork/NeuralNetTest.java
+++ src/test/java/kraemericaIndustries/neuralnetwork/NeuralNetTest.java
@@ -11,6 +11,16 @@ import kraemericaIndustries.matrix.Matrix;
 public class NeuralNetTest {
 
 	private Random random = new Random();
+	
+	@Test
+	public void testTrainEngine() {
+		int inputRows = 5;
+		int cols = 6;
+		int outputRows = 7;
+		
+		Matrix input = Util.generateInputMatrix(inputRows, cols);
+		Matrix expected = Util.generateTrainableExpectedMatrix(outputRows, input);
+	}
 
 	@Test
 	public void testWeightGradient() {
