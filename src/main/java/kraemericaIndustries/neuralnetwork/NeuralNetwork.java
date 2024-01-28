5d71b122bfd583197c1595eccd6dce22f6d4d750
Bob Kraemer <bob.kraemer.android@gmail.com>
2024-01-28 06:59:21 -0500

    Verified code working through lecture 146: "The Neural Network Class"

---
+++ src/main/java/kraemericaIndustries/neuralnetwork/NeuralNetwork.java
@@ -0,0 +1,19 @@
+package kraemericaIndustries.neuralnetwork;
+
+public class NeuralNetwork {
+
+	private Engine engine;
+	
+	public NeuralNetwork() {
+		engine = new Engine();
+	}
+	
+	public void add(Transform transform, double... params) {
+		engine.add(transform, params);
+	}
+
+	@Override
+	public String toString() {
+		return engine.toString();
+	}
+}
