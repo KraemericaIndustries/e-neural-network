3f44eec78ae80cb354fe0d97e4dab840f907d54d
KraemericaIndustries <bob.kraemer.git@gmail.com>
2024-08-09 12:06:16 -0400

    Verified code working through lecture 185: "Determining Canvas Width"

--- src/main/java/kraemericaIndustries/neuralnetwork/loader/image/ImageWriter.java
+++ src/main/java/kraemericaIndustries/neuralnetwork/loader/image/ImageWriter.java
@@ -1,10 +1,12 @@
 package kraemericaIndustries.neuralnetwork.loader.image;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import kraemericaIndustries.neuralnetwork.loader.BatchData;
-import kraemericaIndustries.neuralnetwork.loader.Loader;
-import kraemericaIndustries.neuralnetwork.loader.MetaData;
 
 public class ImageWriter {
 
@@ -27,24 +29,52 @@ public class ImageWriter {
 	
 	public void run(String directory) {
 		
-		System.out.println(directory);
-		
 		final String trainImages = String.format("%s%s%s",  directory, File.separator, "train-images-idx3-ubyte");
 		final String trainLabels = String.format("%s%s%s",  directory, File.separator, "train-labels-idx1-ubyte");
 		final String testImages = String.format("%s%s%s",  directory, File.separator, "t10k-images-idx3-ubyte");
 		final String testLabels = String.format("%s%s%s",  directory, File.separator, "t10k-labels-idx1-ubyte");
 		
-		Loader trainLoader = new ImageLoader(trainImages, trainLabels, 32);
-		Loader testLoader = new ImageLoader(testImages, testLabels, 32);
+		int batchSize = 900; 
 		
-		trainLoader.open();
-		MetaData metaData = testLoader.open();
+		ImageLoader trainLoader = new ImageLoader(trainImages, trainLabels, batchSize);
+		ImageLoader testLoader = new ImageLoader(testImages, testLabels, batchSize);
+		
+		ImageLoader loader = testLoader;
+		
+		ImageMetaData metaData = loader.open();
+		
+		int imageWidth = metaData.getWidth();
+		int imageHeight = metaData.getHeight();
 		
 		for(int i = 0; i < metaData.getNumberBatches(); i++) {
 			BatchData batchData = testLoader.readBatch();
+			
+			var numberImages = metaData.getItemsRead();
+			
+			int horizontalImages = (int)Math.sqrt(numberImages);
+			
+			while(numberImages % horizontalImages != 0) {
+				++horizontalImages;
+			}
+			
+			int verticalImages = numberImages / horizontalImages;
+			
+			int canvasWidth = horizontalImages * imageWidth;
+			int canvasHeight = verticalImages * imageHeight;
+			
+			String montagePath = String.format("montage%d.jpg", i);
+			System.out.println("Writing " + montagePath);
+			
+			var montage = new BufferedImage(canvasWidth, canvasHeight, BufferedImage.TYPE_BYTE_GRAY);
+			
+			try {
+				ImageIO.write(montage, "jpg", new File(montagePath));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
-		trainLoader.close();
-		testLoader.close();
+		loader.close();
 	}
 }
